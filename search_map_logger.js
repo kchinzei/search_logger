145d2278aaa3a4a5005e3701c274b483f1bd3060
kchinzei <k.chinzei@aist.go.jp>
2025-05-09 14:09:03 +0900

    replace '+' to space

--- search_map_logger.js
+++ search_map_logger.js
@@ -1,21 +1,25 @@
 // ==UserScript==
 // @name         Search Map Logger
 // @description  https://github.com/kchinzei/search_logger.git
-// @version      0.5.0
+// @version      0.5.1
 // @match        https://www.google.com/maps/place*
 // @match        https://www.google.co.jp/maps/place*
+// @match        https://www.google.com/maps/search*
+// @match        https://www.google.co.jp/maps/search*
 // @grant        GM_xmlhttpRequest
 // @connect      localhost
 // ==/UserScript==
 
 (function() {
     const s = window.location.pathname;
-    const m = s.match(/^\/maps\/place\/([^\/]+)/);
+    const m = s.match(/^\/maps\/(place|search)\/([^\/]+)/);
     if (!m) return;
 
-    const query = decodeURIComponent(m[1]);
-    const searchUrl = `${window.location.origin}/maps/place/${encodeURIComponent(query)}`;
-		
+    const query = decodeURIComponent(m[2]).replaceAll('+', ' ');
+    var searchUrl = `${window.location.origin}/maps/${m[1]}/${m[2]}`;
+    const m2 = s.match(/^\/maps\/(?:place|search)\/[^\/]+\/(@[^\/]+)/);
+    if (m2) searchUrl = `${window.location.origin}/maps/${m[1]}/${m[2]}/${m2[1]}`;
+
     GM_xmlhttpRequest({
         method: "POST",
         url: "http://localhost:27123",
@@ -23,14 +27,14 @@
             "Content-Type": "application/json"
         },
         data: JSON.stringify({
-            query: query,
+            query: query_readable,
             url: searchUrl
         }),
         onload: function(res) {
-            console.log("[Userscript] Sent search to Python:", res.status);
+            console.log("[Search Map Logger] Sent search to Python:", res.status);
         },
         onerror: function(err) {
-            console.error("[Userscript] Failed to send:", err);
+            console.error("[Search Map Logger] Failed to send:", err);
         }
     });
 })();
